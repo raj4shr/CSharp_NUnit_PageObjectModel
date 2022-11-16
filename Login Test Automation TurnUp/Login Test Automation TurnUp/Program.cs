4246b2edd952473bd70d4be676972d24fc5c262d
raj4shr <118330943+raj4shr@users.noreply.github.com>
2022-11-17 10:07:17 +1300

    TurnUp portal sort column in time and materials page

--- Login Test Automation TurnUp/Login Test Automation TurnUp/Program.cs
+++ Login Test Automation TurnUp/Login Test Automation TurnUp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
 
 
 //open web browser
 IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+//driver.Manage().Window.FullScreen();
 
 //launch turnup portal
 driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+//driver.Manage().Window.FullScreen();
+//driver.Manage().Window.Maximize();
 
 //identify username element and send data
 IWebElement loginUsername = driver.FindElement(By.Id("UserName"));
@@ -20,7 +25,7 @@ loginPassword.SendKeys("123123");
 //.identify login button and click login to enter main page
 IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\'loginForm\']/form/div[3]/input[1]"));
 loginButton.Click();
-driver.Manage().Window.Maximize();
+//driver.Manage().Window.Maximize();
 
 //identifying an element in the login home page to confirm if our test passed
 IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\'logoutForm\']/ul/li/a"));
@@ -31,10 +36,27 @@ else
     Console.WriteLine("Test Failed");
 
 //Identifying Administration menu item and clicking
-IWebElement administrationMenuItem = driver.FindElement(By.LinkText("Administration"));
+//IWebElement administrationMenuItem = driver.FindElement(By.LinkText("Administration"));//by link text
+IWebElement administrationMenuItem = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));//by full xpath
 administrationMenuItem.Click();
 
 //Identifying time and material menu item and clicking
 
-IWebElement timeAndMaterial = driver.FindElement(By.LinkText("Time & Materials"));
+//IWebElement timeAndMaterial = driver.FindElement(By.LinkText("Time & Materials")); //by link text
+//IWebElement timeAndMaterial = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));//by full xpath
+IWebElement timeAndMaterial = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
 timeAndMaterial.Click();
+
+//to click and sort the Code tab in time and material pagfe
+//IWebElement codeTab = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[2]/div/table/thead/tr/th[1]/a"));
+//codeTab.Click();
+
+//to click and sort using the typecode in time and material page
+IWebElement typeCode = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[2]/div/table/thead/tr/th[2]/a"));
+//Thread.Sleep(5000);
+typeCode.Click();
+
+IWebElement tableGrid = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[3]/table"));
+Console.WriteLine("just checking");
+
+Console.WriteLine(driver.FindElements(By.XPath("/html/body/div[4]/div/div/div[3]/table")));
