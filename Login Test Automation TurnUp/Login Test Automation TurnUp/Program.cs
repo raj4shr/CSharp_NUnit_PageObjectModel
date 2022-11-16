dd432a13e6b2a1dbce2dc21f76913c17044c83ae
raj4shr <118330943+raj4shr@users.noreply.github.com>
2022-11-17 01:32:20 +1300

    IC login portal login and time & material

---
+++ Login Test Automation TurnUp/Login Test Automation TurnUp/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+//open web browser
+IWebDriver driver = new ChromeDriver();
+
+//launch turnup portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//identify username element and send data
+IWebElement loginUsername = driver.FindElement(By.Id("UserName"));
+loginUsername.SendKeys("hari");
+
+//identify password element and send data
+IWebElement loginPassword = driver.FindElement(By.Id("Password"));
+loginPassword.SendKeys("123123");
+
+
+//.identify login button and click login to enter main page
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\'loginForm\']/form/div[3]/input[1]"));
+loginButton.Click();
+driver.Manage().Window.Maximize();
+
+//identifying an element in the login home page to confirm if our test passed
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\'logoutForm\']/ul/li/a"));
+
+if (helloHari.Displayed == true)
+    Console.WriteLine("Test Passed");
+else
+    Console.WriteLine("Test Failed");
+
+//Identifying Administration menu item and clicking
+IWebElement administrationMenuItem = driver.FindElement(By.LinkText("Administration"));
+administrationMenuItem.Click();
+
+//Identifying time and material menu item and clicking
+
+IWebElement timeAndMaterial = driver.FindElement(By.LinkText("Time & Materials"));
+timeAndMaterial.Click();
