ec836c0d89d2f5372d3e6fe70ba1f7ff610f0c39
raj4shr <118330943+raj4shr@users.noreply.github.com>
2022-11-17 16:31:23 +1300

    complete TurnUp portal automation for login and time and materials page

--- Login Test Automation TurnUp/Login Test Automation TurnUp/Program.cs
+++ Login Test Automation TurnUp/Login Test Automation TurnUp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System.Collections.ObjectModel;
 
 
@@ -10,9 +11,9 @@ driver.Manage().Window.Maximize();
 
 //launch turnup portal
 driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
-//driver.Manage().Window.FullScreen();
-//driver.Manage().Window.Maximize();
+
 Thread.Sleep(2000);
+
 //identify username element and send data
 IWebElement loginUsername = driver.FindElement(By.Id("UserName"));
 loginUsername.SendKeys("hari");
@@ -71,10 +72,51 @@ for (int i = 1; i <= rows.Count(); i++)
     for (int j = 1; j <= 4; j++)
     {
         //Thread.Sleep(2000);
-        string str = "//*[@id=\\\"tmsGrid\\\"]/div[3]/table/tbody/tr[" + i + "]/td[" + j + "]";
+        //string str = "//*[@id=\\\"tmsGrid\\\"]/div[3]/table/tbody/tr[" + i + "]/td[" + j + "]";
         IWebElement columns = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[" + i + "]/td[" + j + "]"));
         Console.Write("     " + columns.Text + "     ");
     }
     
 }
+
+//clicking page number 2
+IWebElement nextPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/ul/li[2]/a"));
+nextPage.Click();
+//driver.SwitchTo().Alert().Accept();
+
+//clicking the items per page drop down box
+IWebElement itemsPerPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/span[1]/span/span/span[2]/span"));
+itemsPerPage.Click();
+
+//selecting 20 items per page from the drop down box
+IWebElement selectItemsPerPage = driver.FindElement(By.XPath("/html/body/div[5]/div/ul/li[3]"));
+selectItemsPerPage.Click();
+
+//dragging a column header and droping in the tab to group by columns
+IWebElement dragHeaderElement = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[2]/div/table/thead/tr/th[1]/a"));
+//dragHeaderGroupBy.
+IWebElement dropIntoElement = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[1]"));
+
+//selenium actions class object created for drag and drop operation
+Actions builder = new Actions(driver);
+
+builder.DragAndDrop(dragHeaderElement, dropIntoElement).Perform();
+
+Thread.Sleep(5000);
+
+//closing the drag and drop column using full xpath(normal xpath won't work)
+IWebElement closeDragAndDropColumn = driver.FindElement(By.XPath("/html/body/div[4]/div/div/div[1]/div/a[2]/span"));
+closeDragAndDropColumn.Click();
+
+//logging off from the portal
+helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+helloHari.Click();
+IWebElement logOff = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/ul/li[2]/a"));
+logOff.Click();
+
+Thread.Sleep(2000);
+//closing the chrome browser
+driver.Close();
+
+
 
